22ed45099c36bdd349fc6e4a348c250a3aeb3a3e
Nik <zr4x@ya.ru>
2019-03-13 23:11:57 +0500

    home work 3

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,7 @@
 public class MainClass {
 
     private int class_number = 20;
+    private String class_string = "Hello, orld";
 
     public static int getLocalNumber() {
         return 14;
@@ -9,4 +10,6 @@ public class MainClass {
     public int getClassNumber(){
         return this.class_number;
     }
+
+    public String getClassString() { return  this.class_string; }
 }
