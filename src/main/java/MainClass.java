0bd6abbc7c42ba3806af99eab3a77069031e8c28
Nik <zr4x@ya.ru>
2019-03-13 22:55:42 +0500

    home work 2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,12 @@
 public class MainClass {
 
+    private int class_number = 20;
+
     public static int getLocalNumber() {
         return 14;
+    }
+
+    public int getClassNumber(){
+        return this.class_number;
     }
 }
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,11 +1,17 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainClassTest {
 
     @Test
-    public void testGetLocalNumber(){
+    void testGetLocalNumber(){
         assertEquals(14, MainClass.getLocalNumber(), "Local number return wrong number ");
+    }
+
+    @Test
+    void testGetClassNumber() {
+        assertTrue(new MainClass().getClassNumber() > 45, "getClassNumber return number less than 45");
     }
 }
