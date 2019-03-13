42a602463f7ec9df112b5bb75632d9f6b40ecc99
Nik <zr4x@ya.ru>
2019-03-13 23:13:29 +0500

    home work 3

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -5,13 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainClassTest {
 
+    //Home Work #1
     @Test
     void testGetLocalNumber(){
         assertEquals(14, MainClass.getLocalNumber(), "Local number return wrong number ");
     }
 
+    //Home Work #2
     @Test
     void testGetClassNumber() {
         assertTrue(new MainClass().getClassNumber() > 45, "getClassNumber return number less than 45");
+    }
+
+    //Home Work #3
+    @Test
+    void testGetClassString() {
+        String text = new MainClass().getClassString();
+        assertTrue(text.contains("hello") || text.contains("Hello"), "class_string doesn't contains «hello» or «Hello»");
     }
 }
